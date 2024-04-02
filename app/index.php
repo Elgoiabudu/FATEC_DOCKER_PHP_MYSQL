af20ebd254c4d8f7985ad5d2e89947525d368a27
Fernando Bonde Leonelli <117871790+Elgoiabudu@users.noreply.github.com>
2024-04-02 00:15:30 +0000

    Inclusão de comentários ilustrativos

--- app/index.php
+++ app/index.php
@@ -14,11 +14,15 @@
         <?php
         $pdo = new PDO('mysql:host=mysql;dbname=test', 'root', 'password');
 
+        //Apenas para teste. Não é uma boa prática
         $pdo->exec("CREATE TABLE IF NOT EXISTS usuarios (id int AUTO_INCREMENT PRIMARY KEY, nome VARCHAR(255), email VARCHAR(255))");
+        //Inserindo um registro
         $pdo->exec("INSERT INTO usuarios (id, nome, email) VALUES (NULL, 'Teste', 'teste@email.com')");
 
+        //Lendo os registros
         $stmt = $pdo->query('SELECT * FROM usuarios;');
 
+        //Preenchendo a tabela com os dados do Sql
         while ($row = $stmt->fetch()) {
             echo "<tr><td>{$row['id']}</td><td>{$row['nome']}</td><td>{$row['email']}</td></tr>";
         }
